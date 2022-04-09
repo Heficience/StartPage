9e93598d27aee76e0f5a872fc6469f3af92c41d0
Paul WOISARD <paulwoisard@gmail.com>
2022-04-09 21:16:53 +0200

    first commit

---
+++ search.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Startpage</title>
+  <link rel="stylesheet" href="color.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="./assets/fontawesome/css/all.css">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body class="preload">
+  <div id="container">
+    <div id="main">
+      <div class="logo" id="logo">
+        <img src="assets/logo.svg">
+      </div>
+      <div class="circletag" id="nay">
+        <img src="assets/google.png">
+      </div>
+      <div class="searchbar">
+        <input id="search" type="text" placeholder="Rechercher sur Internet...">
+      </div>
+      <div class="links">
+        <div class="border">
+          <a href="https://heficience.com/index.php"><img src="assets/heficience.png" width="30px" height="30px" style="vertical-align: bottom;padding-bottom: 3px;"></a>
+          <a href="https://github.com/Heficience"><i class="fab fa-github"></i></a>
+          <a href="https://www.facebook.com/Heficience"><i class="fab fa-facebook"></i></a>
+          <a href="https://twitter.com/heficience"><i class="fab fa-twitter"></i></a>
+        </div>
+      </div>
+    </div>
+    <footer>Made with <i class="fa fa-heart"></i>, by Nimplex, Updated by Paullux.</footer>
+  </div>
+  <script src="script.js"></script>
+</body>
+</html>
