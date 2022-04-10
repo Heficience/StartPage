846c29e02d83f97a3af14e201815fde9f778db74
Paul WOISARD <paulwoisard@gmail.com>
2022-04-10 19:24:50 +0200

    Un peu plus Responsive

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const searchBox = document.getElementById('search')
 
 searchBox.addEventListener('keydown', (e) => {
-  if (e.code == 'Enter') {
+  if (e.code == 'Enter' || e.code == 'NumpadEnter') {
     const { value } = searchBox
     if (value.replace(/ /gm, '').length > 0) location.href = `https://www.google.com/search?q=${value}`
     else searchBox.value = ''
