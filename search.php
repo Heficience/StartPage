79df8b1d7275eda195c083cc8fb126b092033e56
Paul WOISARD <paulwoisard@gmail.com>
2022-04-18 13:03:24 +0200

    changement logo

--- search.php
+++ search.php
@@ -3,23 +3,26 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Startpage</title>
+  <title>StartPage</title>
   <link rel="stylesheet" href="color.css">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="./assets/fontawesome/css/all.css">
-  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <link rel="icon" type="image/png" href="favicon.png">
 </head>
 <body class="preload">
   <div id="container">
     <div id="main">
-      <div class="logo" id="logo">
+      <!--<div class="logo" id="logo">
         <img src="assets/logo.svg">
+      </div>-->
+      <div class="StartPage" id="StartPage">
+        <img src="assets/StartPage.svg">
       </div>
       <div class="circletag" id="nay">
         <img src="assets/google.png">
       </div>
       <div class="searchbar">
-        <input id="search" type="text" placeholder="Rechercher sur Internet...">
+        <input id="search" type="text" placeholder="Rechercher sur Internet..."  autocomplete="off" >
       </div>
       <div class="links">
         <div class="border">
